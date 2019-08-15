965c5e899c86195a62b6e74ce907b776c8633737
Edson Ayllon <EAYLLON1@GMAIL.COM>
2019-08-15 23:40:34 +0000

    added static build

---
+++ static-website/precache-manifest.6b605c4379725ea3f504d5d50ef47f43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0779710cc6179bda058ac138a311bd63",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dd1d96960f41d7d22c1a",
+    "url": "./static/css/main.f13454b3.chunk.css"
+  },
+  {
+    "revision": "ddc8b05b69fcca4d6d33",
+    "url": "./static/js/2.7ac25155.chunk.js"
+  },
+  {
+    "revision": "dd1d96960f41d7d22c1a",
+    "url": "./static/js/main.0afb9a76.chunk.js"
+  },
+  {
+    "revision": "15e951923b93309d5d16",
+    "url": "./static/js/runtime~main.1dae895e.js"
+  }
+]);
