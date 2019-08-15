51dfb1ceb59f5b7d63f237bacc9270c304d02d5c
Edson Ayllon <EAYLLON1@GMAIL.COM>
2019-08-15 23:10:20 +0000

    finish html css

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <h1>Welcome to the world's first decntralized arts organization.</h1>
+        <header style={{height: 100}}>
+          <img src={logo} style={{float: 'left', margin: 20}}/>
+        </header>
+        <div className="content">
+          <h1>Welcome to the world's first decntralized arts organization.</h1>
 
-        <p>What makes something original?</p>
-        <input type="text" />
-
-        <a className="button">Join</a>
-
-        <p>
-          Your donation, along with your answer above will be interpreted by the
-          Ethereum Virtual Machine and used to generate a verifiably unique
-          and digitally-rare art piece that will belong to you
-        </p>
-        <p>
-          This token establishes you as one of the founding patrons of
-        </p>
-        <h2 className="daovinchi">DAO VINCI</h2>
-
+          <p>What makes something original?</p>
+          <textarea name="Text1" cols="50" rows="5" placeholder="Type your answer here"></textarea>
+          <br/>
+          <a className="button">Join</a>
+          <p>
+            Your donation, along with your answer above will be interpreted by the
+            Ethereum Virtual Machine and used to generate a verifiably unique
+            and digitally-rare art piece that will belong to you
+          </p>
+          <p>
+            This token establishes you as one of the founding patrons of
+          </p>
+          <h2 className="daovinchi">DAO VINCI</h2>
+        </div>
     </div>
   );
 }
